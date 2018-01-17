6c454aa8760da04b40bedf799c4f192531854781
陈德伟 <chendewei@cmhi.chinamobile.com>
2018-01-17 13:01:24 +0800

    chapter4 commit

---
+++ src/main/java/org/chen/practice/Chapter4.java
@@ -0,0 +1,24 @@
+package org.chen.practice;
+
+/**
+ * @author chendewei2401@gmail.com
+ * @version V1.0
+ * @date 2018年1月17日 下午1:34:02
+ * @since JDK1.8
+ * <p>
+ * 控制执行流程:
+ *
+ * 1，foreach是Java5中引入的一种新的for语法，用于数组和容器
+ * 2，如果在返回void的方法中没有return语句，方法的结尾处其实会有一个隐式的return
+ * 3，switch的参数为整数类型或者String类型
+ *
+ *
+ */
+public class Chapter4 {
+
+    public static void main(String[] args) {
+
+
+    }
+
+}
---
+++ src/main/java/org/chen/practice/Chapter5.java
@@ -0,0 +1,22 @@
+package org.chen.practice;
+
+/**
+ * @author chendewei2401@gmail.com
+ * @version V1.0
+ * @date 2018年1月18日 下午1:34:02
+ * @since JDK1.8
+ * <p>
+ * 初始化与清理:
+ *
+ * 1，
+ *
+ *
+ */
+public class Chapter5 {
+
+    public static void main(String[] args) {
+
+
+    }
+
+}

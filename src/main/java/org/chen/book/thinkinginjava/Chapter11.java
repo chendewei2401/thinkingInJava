748a049c4d2538f844cb6a16c24565d81fde33b5
陈德伟 <chendewei@cmhi.chinamobile.com>
2018-02-06 17:24:15 +0800

    string sample add

---
+++ src/main/java/org/chen/book/thinkinginjava/Chapter11.java
@@ -0,0 +1,20 @@
+package org.chen.book.thinkinginjava;
+
+/**
+ * @author chendewei2401@gmail.com
+ * @version V1.0
+ * @date 2018年2月5日 下午1:34:02
+ * @since JDK1.8
+ * <p>
+ * 持有对象:
+ * 1，
+ * 4，
+ *
+ */
+public class Chapter11 {
+
+    public static void main(String[] args) {
+
+    }
+
+}
